b9357dc83c0bae4cd7dcc3efb3e9990355a42dd7
diego.lopez <diego.lopez@etermax.com>
2017-05-18 15:00:18 -0300

    Orientation changing supported

--- BoardGamesTimer/TimersViewController.swift
+++ BoardGamesTimer/TimersViewController.swift
@@ -13,6 +13,16 @@ class TimersViewController: UIViewController {
     var jugadores: Int?
     var playerViews = [PlayerView]()
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector (orientationChanged), name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,13 +44,58 @@ class TimersViewController: UIViewController {
 
             let v = PlayerView(color: colors[i], animationColor: animationColors[i], fontColor: fontColors[i])
             playerViews.append(v)
-            view.addSubview(v)
             
             let gesture = UITapGestureRecognizer(target: self, action: #selector (self.didTap(sender:)))
             v.addGestureRecognizer(gesture)
-
+        }
+        
+        orientationChanged()
+    }
+    
+    func addPortraitConstraints() {
+        
+        for i in 0..<jugadores! {
+            
+            let v = playerViews[i]
+            v.removeFromSuperview()
+            view.addSubview(v)
+            
             let topConstraint: NSLayoutConstraint, leftConstraint: NSLayoutConstraint
 
+            if (i < 2) {
+                topConstraint = NSLayoutConstraint(item: v, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 20)
+            }
+            else if (i >= 2 && i < 4) {
+                topConstraint = NSLayoutConstraint(item: v, attribute: .top, relatedBy: .equal, toItem: playerViews[0], attribute: .bottom, multiplier: 1, constant: 0)
+            }
+            else {
+                topConstraint = NSLayoutConstraint(item: v, attribute: .top, relatedBy: .equal, toItem: playerViews[2], attribute: .bottom, multiplier: 1, constant: 0)
+            }
+            
+            if (i % 2 == 0) {
+                leftConstraint = NSLayoutConstraint(item: v, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
+            } else {
+                leftConstraint = NSLayoutConstraint(item: v, attribute: .leading, relatedBy: .equal, toItem: playerViews[0], attribute: .trailing, multiplier: 1, constant: 0)
+            }
+            
+            let widthConstraint = NSLayoutConstraint(item: v, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1/2, constant: 0)
+            let heightConstraint = NSLayoutConstraint(item: v, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1/3, constant: -(20/3))
+            
+            NSLayoutConstraint.activate([topConstraint, leftConstraint, heightConstraint, widthConstraint])
+        }
+        
+    }
+    
+    func addLandscapeConstraints() {
+        
+        for i in 0..<jugadores! {
+            
+            let v = playerViews[i]
+            v.removeFromSuperview()
+            view.addSubview(v)
+            
+            let topConstraint: NSLayoutConstraint, leftConstraint: NSLayoutConstraint
+            
             if (i < 3) {
                 topConstraint = NSLayoutConstraint(item: v, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0)
             } else {
@@ -84,6 +139,15 @@ class TimersViewController: UIViewController {
         }
     }
 
+    func orientationChanged() {
+        if UIDevice.current.orientation.isLandscape {
+            addLandscapeConstraints()
+        }
+        else if UIDevice.current.orientation == .portrait {
+            addPortraitConstraints()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
