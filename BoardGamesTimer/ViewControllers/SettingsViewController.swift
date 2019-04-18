6abc8d5a1617dc3761048e17a0a9d49978a25662
Diego López Bugna <diego.lopezbugna@mercadolibre.com>
2019-04-17 23:59:00 -0300

    settings text view localization

--- BoardGamesTimer/ViewControllers/SettingsViewController.swift
+++ BoardGamesTimer/ViewControllers/SettingsViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet private var usernameTextField: UITextField!
-
+    @IBOutlet private var mainTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -20,6 +21,8 @@ class SettingsViewController: UIViewController {
         self.usernameTextField.addTarget(self, action: #selector(self.usernameEditingEnd), for: .editingDidEndOnExit)
 
         self.usernameTextField.text = UserDefaults.standard.value(forKey: UserDefaults.Keys.bggUsername.rawValue) as? String
+        
+        self.mainTextView.text = "Board Games Timer connects with Board Game Geek to get user logged plays. \n\nIn a near future, Board Games Timer saved plays will be syncronized with BGG logged plays!".localized
     }
     
     @objc func usernameEditingEnd() {
